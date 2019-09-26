b496ecd409633a214d079055ce522f3c090f0b32
gaganVirk <deep.virk@outlook.co.nz>
2019-09-26 16:20:26 +1200

    Created Rad and Point classes to find points in convex hull

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvexHullFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader stdIn = Console.In;
+            Console.SetIn(new StreamReader("data.txt"));
+
+            List<Point> points = new List<Point>();
+            List<Point> hull = new List<Point>();
+            Point pivot = new Point(int.MaxValue, int.MaxValue);
+
+            int k = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] vertices = input.Split(' ');
+                int v1 = int.Parse(vertices[0]);
+                int v2 = int.Parse(vertices[1]);
+
+                input = Console.ReadLine();
+            }
+
+            bool isValidHull()
+            {
+                if(hull.Count < 3)
+                {
+                    return true;
+                }
+                Point a = hull[hull.Count - 3];
+                Point b = hull[hull.Count - 2];
+                Point c = hull[hull.Count - 1];
+                int cw = a.X * b.Y - b.X * a.Y + b.X * c.Y - c.X * b.Y + c.X * a.Y - a.X * c.Y;
+                return cw > 0;
+            }
+
+            Console.SetIn(stdIn);
+            Console.ReadLine();
+        }
+    }
+}
