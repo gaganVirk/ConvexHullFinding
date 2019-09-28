dd1392309c5c3e37dbe7b2c87f84c3623778bd9b
gaganVirk <deep.virk@outlook.co.nz>
2019-09-28 19:56:34 +1200

    program reads different data set

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,10 +19,9 @@ namespace ConvexHullFinding
             List<Point> hull = new List<Point>();
             Point pivot = new Point(int.MaxValue, int.MaxValue);
 
-            int times = 6;
-            while (times <= 6)
+            int dataSet = int.Parse(Console.ReadLine());
+            for(int j = 0; j < dataSet; j++)
             {
-                int k = int.Parse(Console.ReadLine());
                 int n = int.Parse(Console.ReadLine());
 
                 for (int i = 0; i < n; i++)
@@ -30,7 +30,7 @@ namespace ConvexHullFinding
                     string[] vertices = input.Split(' ');
                     int v1 = int.Parse(vertices[0]);
                     int v2 = int.Parse(vertices[1]);
-
+                    Console.WriteLine("V1: {0} V2: {1}", v1, v2);
                     if (pivot.X > v1)
                     {
                         if (pivot.Y > v1)
@@ -40,10 +40,18 @@ namespace ConvexHullFinding
                     }
                     points.Add(new Point(v1, v2));
                 }
+                int neg = int.Parse(Console.ReadLine());
+                if(neg == -1)
+                {
+                    Console.WriteLine(neg);
+                }
+
             }
 
+
+
             points.Sort(new Rad(pivot));
-            Console.WriteLine(pivot);
+            Console.WriteLine("Pivot is" +pivot);
 
             bool isValidHull()
             {
